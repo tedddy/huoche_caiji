3bf740048e54df895d8a5489b9b2cf786e64e6ff
tedddy <tedddy@users.noreply.github.com>
2014-09-13 19:45:21 +0800

    PROCEDURE_shujuzhengli_mjyx
    新文件：数据整理

---
+++ PROCEDURE_shujuzhengli_mjyx.sql
@@ -0,0 +1,30 @@
+-- --------------------------------------------------------------------------------
+-- Routine DDL
+-- Note: comments before and after the routine body will not be stored by the server
+-- --------------------------------------------------------------------------------
+DELIMITER $$
+
+CREATE DEFINER=`root`@`%` PROCEDURE `shujuzhengli_mjyx`()
+BEGIN
+        INSERT INTO `jd_mjyx` (`jd_id`, `jd_mjyx_id`, `jd_mjyx_biaoqian`, `jd_mjyx_shuliang`) 
+			(SELECT 
+					`jd_id`,
+					SUBSTRING_INDEX(`jd_mjyxhz_01`, '~', 1),
+					REPLACE(SUBSTRING_INDEX(`jd_mjyxhz_01`, '~', 2),
+						CONCAT(SUBSTRING_INDEX(`jd_mjyxhz_01`, '~', 1),
+								'~'),
+						''),
+					REPLACE(SUBSTRING_INDEX(`jd_mjyxhz_01`, '~', 3),
+						CONCAT(SUBSTRING_INDEX(`jd_mjyxhz_01`, '~', 2),
+								'~'),
+						'')
+			FROM
+					`jd_mjyxhz`)
+			ON DUPLICATE KEY UPDATE 
+					`jd_mjyx_shuliang`= (
+											REPLACE(SUBSTRING_INDEX(`jd_mjyxhz_01`, '~', 3),
+												CONCAT(SUBSTRING_INDEX(`jd_mjyxhz_01`, '~', 2),
+														'~'),
+												'')
+										);
+END
