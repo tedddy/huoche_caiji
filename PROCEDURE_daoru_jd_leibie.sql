956bb19e0bebb3d572c69f160c7e03573baf6b74
tedddy <tedddy@users.noreply.github.com>
2014-09-16 23:28:57 +0800

    Create Procedure daoru_jd_leibie

---
+++ PROCEDURE_daoru_jd_leibie.sql
@@ -0,0 +1,25 @@
+USE `locoy`;
+DROP procedure IF EXISTS `daoru_jd_leibie`;
+
+DELIMITER $$
+USE `locoy`$$
+CREATE DEFINER=`root`@`%` PROCEDURE `daoru_jd_leibie`()
+BEGIN
+
+INSERT INTO `leibie` ( `jd_leibie_id` , `jd_leibie`, `jd_leibie_id1`, `jd_leibie_id0` ) 
+	(SELECT DISTINCT
+			`daoru_jd`.`jd_leibie_id`, 
+			`daoru_jd`.`jd_leibie`,
+			`daoru_jd`.`jd_leibie_id1`,
+			`daoru_jd`.`jd_leibie_id0` 
+	FROM
+			`daoru_jd`
+	WHERE
+			`daoru_jd`.`jd_leibie_id` IS NOT NULL
+	)
+	ON DUPLICATE KEY UPDATE 
+			`jd_leibie`= `daoru_jd`.`jd_leibie`;
+
+END$$
+
+DELIMITER ;
