6221a159e72b07e10eadd87af0c2ccf757b46459
tedddy <tedddy@users.noreply.github.com>
2014-09-17 10:54:29 +0800

    Revert "revise"
    This reverts commit f3101666e98410dc81ff3e4c0a06b121c4e869f1.

--- PROCEDURE_daoru_jd_leibie.sql
+++ PROCEDURE_daoru_jd_leibie.sql
@@ -1,9 +1,8 @@
--- --------------------------------------------------------------------------------
--- Routine DDL
--- Note: comments before and after the routine body will not be stored by the server
--- --------------------------------------------------------------------------------
-DELIMITER $$
+USE `locoy`;
+DROP procedure IF EXISTS `daoru_jd_leibie`;
 
+DELIMITER $$
+USE `locoy`$$
 CREATE DEFINER=`root`@`%` PROCEDURE `daoru_jd_leibie`()
 BEGIN
 
@@ -16,12 +15,11 @@ INSERT INTO `leibie` ( `jd_leibie_id` , `jd_leibie`, `jd_leibie_id1`, `jd_leibie
 	FROM
 			`daoru_jd`
 	WHERE
-			`daoru_jd`.`jd_leibie_id` IS NOT NULL AND
-			`daoru_jd`.`jd_leibie` IS NOT NULL AND
-			`daoru_jd`.`jd_leibie_id1` IS NOT NULL AND
-			`daoru_jd`.`jd_leibie_id0` IS NOT NULL
+			`daoru_jd`.`jd_leibie_id` IS NOT NULL
 	)
 	ON DUPLICATE KEY UPDATE 
 			`jd_leibie`= `daoru_jd`.`jd_leibie`;
 
-END
+END$$
+
+DELIMITER ;
