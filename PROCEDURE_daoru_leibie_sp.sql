12ca3a3d03d095bc130e84f752672c9bf6937383
tedddy <tedddy@users.noreply.github.com>
2014-09-17 22:54:42 +0800

    create procedure daoru_leibie_sp

---
+++ PROCEDURE_daoru_leibie_sp.sql
@@ -0,0 +1,24 @@
+-- --------------------------------------------------------------------------------
+-- Routine DDL
+-- Note: comments before and after the routine body will not be stored by the server
+-- --------------------------------------------------------------------------------
+DELIMITER $$
+
+/* CALL daoru_leibie_sp('[标签:类别ID]', '[标签:类别名字]', '[标签:祖父类ID]', '[标签:父类ID]') */
+
+CREATE DEFINER=`root`@`%` PROCEDURE `daoru_leibie_sp`(IN jd_leibie_id_hc VARCHAR(255), jd_leibie_hc VARCHAR(255), jd_leibie_id0_hc VARCHAR(255), jd_leibie_id1_hc VARCHAR(255))
+BEGIN
+
+/* 1. 采集的数据都先把数据类型设为 VARCHAR，再用CAST转化为相应的类型。2.#0 IN 参数（Parameter）名字和导入数据库的字段名应该不一样，在这里加上_hc(火车）
+*/
+SET jd_leibie_id_hc = IF(jd_leibie_id_hc = '', NULL, CAST(jd_leibie_id_hc AS UNSIGNED));
+SET jd_leibie_id0_hc = IF(jd_leibie_id0_hc = '', NULL, CAST(jd_leibie_id0_hc AS UNSIGNED));
+SET jd_leibie_id1_hc = IF(jd_leibie_id1_hc = '', NULL, CAST(jd_leibie_id1_hc AS UNSIGNED));
+SET jd_leibie_hc = IF(jd_leibie_hc = '' OR jd_leibie_hc LIKE '%???%' OR jd_leibie_hc LIKE '%db:%', NULL, jd_leibie_hc);
+
+INSERT INTO `leibie` (`jd_leibie_id`, `jd_leibie_id0`, `jd_leibie_id1`, `jd_leibie`) VALUES (jd_leibie_id_hc, jd_leibie_id0_hc, jd_leibie_id1_hc, jd_leibie_hc) 
+ON DUPLICATE KEY UPDATE 
+	`jd_leibie_id1` = IF(jd_leibie_id1 IS NULL, `jd_leibie_id1`, jd_leibie_id0_hc),
+	`jd_leibie_id0` = IF(jd_leibie_id0 IS NULL, `jd_leibie_id0`, jd_leibie_id0_hc),
+	`jd_leibie` = IF(jd_leibie IS NULL, `jd_leibie`, jd_leibie_hc);
+END
