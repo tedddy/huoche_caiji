1123bdca8177c274df5e9edeba42be31121dc096
tedddy <tedddy@users.noreply.github.com>
2014-09-20 18:29:21 +0800

    修改 Procedure daoru_leibie_tj_sp

--- PROCEDURE_daoru_leibie_tj_sp.sql
+++ PROCEDURE_daoru_leibie_tj_sp.sql
@@ -18,11 +18,27 @@ BEGIN
 SET jd_leibie_id_hc = IF(jd_leibie_id_hc = '', NULL, CAST(jd_leibie_id_hc AS UNSIGNED));
 SET jd_leibie_zs_hc = IF(jd_leibie_zs_hc = '', NULL, CAST(jd_leibie_zs_hc AS UNSIGNED));
 SET jd_leibie_fys_hc = IF(jd_leibie_fys_hc = '', NULL, CAST(jd_leibie_fys_hc AS UNSIGNED));
+SET jd_flphb1_hc = IF(jd_flphb1_hc = 'jd_flphb1_hc', NULL, jd_flphb1_hc);
+SET jd_flphb2_hc = IF(jd_flphb2_hc = 'jd_flphb2_hc', NULL, jd_flphb2_hc);
+SET jd_flphb3_hc = IF(jd_flphb3_hc = 'jd_flphb3_hc', NULL, jd_flphb3_hc);
+SET jd_flphb4_hc = IF(jd_flphb4_hc = 'jd_flphb4_hc', NULL, jd_flphb4_hc);
+SET jd_flphb5_hc = IF(jd_flphb5_hc = 'jd_flphb5_hc', NULL, jd_flphb5_hc);
+SET jd_flphb6_hc = IF(jd_flphb6_hc = 'jd_flphb6_hc', NULL, jd_flphb6_hc);
+SET jd_flphb7_hc = IF(jd_flphb7_hc = 'jd_flphb7_hc', NULL, jd_flphb7_hc);
+SET jd_flphb8_hc = IF(jd_flphb8_hc = 'jd_flphb8_hc', NULL, jd_flphb8_hc);
 SET jd_jintian_riqi_hc = IF(jd_jintian_riqi_hc = '', NULL, jd_jintian_riqi_hc);/*这个不用CAST，因为导入的时候，数据类型就是DATE*/
 
 INSERT INTO `leibie` (`jd_leibie_id`, `jd_leibie_zs`, `jd_leibie_fys`, jd_jintian_riqi) VALUES (jd_leibie_id_hc, jd_leibie_zs_hc, jd_leibie_fys_hc, jd_jintian_riqi_hc) 
 ON DUPLICATE KEY UPDATE 	
 	`jd_leibie_zs` = IF(jd_leibie_zs_hc IS NULL, `jd_leibie_zs`, jd_leibie_zs_hc),
 	`jd_leibie_fys` = IF(jd_leibie_fys_hc IS NULL, `jd_leibie_fys`, jd_leibie_fys_hc),
+	`jd_flphb1` = IF(jd_flphb1_hc IS NULL, `jd_flphb1`, jd_flphb1_hc),
+	`jd_flphb2` = IF(jd_flphb2_hc IS NULL, `jd_flphb2`, jd_flphb2_hc),
+	`jd_flphb3` = IF(jd_flphb3_hc IS NULL, `jd_flphb3`, jd_flphb3_hc),
+	`jd_flphb4` = IF(jd_flphb4_hc IS NULL, `jd_flphb4`, jd_flphb4_hc),
+	`jd_flphb5` = IF(jd_flphb5_hc IS NULL, `jd_flphb5`, jd_flphb5_hc),
+	`jd_flphb6` = IF(jd_flphb6_hc IS NULL, `jd_flphb6`, jd_flphb6_hc),
+	`jd_flphb7` = IF(jd_flphb7_hc IS NULL, `jd_flphb7`, jd_flphb7_hc),
+	`jd_flphb8` = IF(jd_flphb8_hc IS NULL, `jd_flphb8`, jd_flphb8_hc),
 	`jd_jintian_riqi` = IF(jd_jintian_riqi_hc IS NULL, `jd_jintian_riqi`, jd_jintian_riqi_hc);
 END
