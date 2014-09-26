dc7c575e2ce9727a89f49d5bedde61e1ece07cd4
tedddy <tedddy@users.noreply.github.com>
2014-09-26 22:29:28 +0800

    daoru_jd_comment

---
+++ PROCEDURE_daoru_jd_comment.sql
@@ -0,0 +1,25 @@
+USE `locoy`;
+DROP procedure IF EXISTS `daoru_jd_comment`;
+
+DELIMITER $$
+USE `locoy`$$
+CREATE DEFINER=`root`@`%` PROCEDURE `daoru_jd_comment`(IN jd_id_hc VARCHAR(255), jd_pinglunshu_hc VARCHAR(255), jd_haoping_hc VARCHAR(255), jd_zhongping_hc VARCHAR(255), jd_chaping_hc VARCHAR(255), jd_shaidanshu_hc VARCHAR(255), jd_mjyxhz_hc VARCHAR(2555), jd_jintian_riqi_hc DATE)
+BEGIN
+
+/* 
+CALL daoru_jd_comment('[标签:京东货号]', '[标签:评论数]', '[标签:好评数]', '[标签:中评数]', '[标签:差评数]', '[标签:晒单数]', '[标签:买家印象]', '[系统时间转化:yyyy-MM-dd]') 
+*/
+
+SET jd_id_hc = IF(jd_id_hc = '', NULL, jd_id_hc);
+SET jd_pinglunshu_hc = IF(jd_pinglunshu_hc = '' OR jd_pinglunshu_hc LIKE '%db:%', NULL, CAST(jd_pinglunshu_hc AS UNSIGNED));
+SET jd_haoping_hc = IF(jd_haoping_hc = '' OR jd_haoping_hc LIKE '%db:%', NULL, CAST(jd_haoping_hc AS UNSIGNED));
+SET jd_zhongping_hc = IF(jd_zhongping_hc = '' OR jd_zhongping_hc LIKE '%db:%', NULL, CAST(jd_zhongping_hc AS UNSIGNED));
+SET jd_chaping_hc = IF(jd_chaping_hc = '' OR jd_chaping_hc LIKE '%db:%', NULL, CAST(jd_chaping_hc AS UNSIGNED));
+SET jd_shaidanshu_hc = IF(jd_shaidanshu_hc = '' OR jd_shaidanshu_hc LIKE '%db:%', NULL, CAST(jd_shaidanshu_hc AS UNSIGNED));
+SET jd_mjyxhz_hc = IF(jd_mjyxhz_hc = '' OR jd_mjyxhz_hc LIKE '%???%' OR jd_mjyxhz_hc LIKE '%db:%', NULL, jd_mjyxhz_hc);
+
+INSERT INTO `daoru_jd_comment` (`jd_id`, `jd_pinglunshu`, `jd_haoping`, `jd_zhongping`, `jd_chaping`, `jd_shaidanshu`, jd_mjyxhz, `jd_jintian_riqi`) VALUES (jd_id_hc, jd_pinglunshu_hc, jd_haoping_hc, jd_zhongping_hc, jd_chaping_hc, jd_shaidanshu_hc, jd_mjyxhz_hc, jd_jintian_riqi_hc);
+
+END$$
+
+DELIMITER ;
