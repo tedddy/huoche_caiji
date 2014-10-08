a9457b846bba2d6f6dae77a895e1510cfaa06338
tedddy <tedddy@users.noreply.github.com>
2014-10-08 23:06:14 +0800

    PROCEDURE_daoru_jd_price.sql
    增加 ON DUPLICATE KEY UPDATE

--- PROCEDURE_daoru_jd_price.sql
+++ PROCEDURE_daoru_jd_price.sql
@@ -13,7 +13,8 @@ CALL daoru_jd_price('[采集页网址]', '[标签:现价]', '[系统时间转化
 SET jd_id_hc = SUBSTRING_INDEX(`jd_id_hc`, '_', -1);
 SET jd_xianjia_hc = IF(jd_xianjia_hc = '' OR jd_xianjia_hc LIKE '%db:%', NULL, CAST(jd_xianjia_hc AS DECIMAL));
 
-INSERT INTO `daoru_jd_price` (`jd_id`, `jd_xianjia`, `jd_jintian_riqi`) VALUES (jd_id_hc, jd_xianjia_hc, jd_jintian_riqi_hc);
+INSERT INTO `daoru_jd_price` (`jd_id`, `jd_xianjia`, `jd_jintian_riqi`) VALUES (jd_id_hc, jd_xianjia_hc, jd_jintian_riqi_hc)
+ON DUPLICATE KEY UPDATE `jd_xianjia` = IF(jd_xianjia_hc IS NULL, `jd_xianjia`, jd_xianjia_hc);
 
 END$$
 
